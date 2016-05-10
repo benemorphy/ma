ebe7a4f0b1f9e51eced571ab2106110dd5f5eb88
benemorphy <benemorphy@126.com>
2016-05-10 17:32:20 +0800

    route meeting room

--- client/templates/_loginButtonsAdditionalLoggedInDropdownActions.js
+++ client/templates/_loginButtonsAdditionalLoggedInDropdownActions.js
@@ -1,6 +1,6 @@
 Template._loginButtonsLoggedInDropdown.events({
     'click #login-buttons-edit-profile': function(event) {
-        Router.go('meetingroom',Meteor.user()._id);
+        Router.go('meetingroom',{_id:Meteor.user()._id});
         console.log (Meteor.user()._id)
     }
 });
