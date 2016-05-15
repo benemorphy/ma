3a8c0372861c67ac4804133ec7d29ea2a699ff2e
benemorphy <benemorphy@126.com>
2016-05-15 09:16:55 +0800

    clean 0.01

--- client/subscibe.js
+++
@@ -1,6 +0,0 @@
-//Meteor.subscribe('allscenes');
-//Meteor.subscribe('meetingroom','2c8HmbE85XJSpWXKD');
-//if (Meteor.user())
-//Meteor.subscribe("room_roomname",(Meteor.user().username));
-//
-//Meteor.subscribe("room_scenes",2)
