f4811a2a902d1c0c71d971ed59569f1982414dec
benemorphy <benemorphy@126.com>
2016-06-15 19:21:06 +0800

    wait for debug

--- client/templates/mainboard.js
+++ client/templates/mainboard.js
@@ -9,7 +9,6 @@ Template.mainboard.helpers({
 
 
 Template.mainboard.onRendered(function(){
-///
 var PrintscreenJs = {
     
     option: {
@@ -118,7 +117,39 @@ PrintscreenJs.initialize('img_invisible');
 ///
 var img=document.getElementById('img_invisible');
 img.onload=function(){
-  console.log("img_invisible has loaded src");
+  var img=document.getElementById('img_invisible');
+    //这里尚未添加事件钩子，先用笨办法将图片存入服务器
+
+    var ann_canvas = document.getElementById('canvas_invisible');
+    var paper_ctx=ann_canvas.getContext('2d');
+    var a=(img.width)/(img.height);
+    paper_ctx.drawImage(img,0,0,img.width,img.height,0,0,ann_canvas.height*a,ann_canvas.height);  
+      var dataurl=ann_canvas.toDataURL("image/jpeg",0.7);
+  var name = Scenes.findOne().room_id;
+  var scene_id=Scenes.findOne({type:"current_scene_id"}).current_scene_id
+    var c=Scenes.findOne({type:"scene_background",scene_id:scene_id});
+   if (c)
+    {var id=c._id;
+
+      Scenes.update(
+        {_id:id},
+        {$set:{name: name,
+          backimage: dataurl,
+          room_id:name,
+          time: Date.now()}}
+
+
+          )}
+      else {
+        Scenes.insert({
+          type:"scene_background",
+          scene_id:scene_id,
+          name: name,
+          backimage: dataurl,
+          room_id:name,
+          time: Date.now()
+        })}
+
 
 };
 })
